73d56f5328d5e1a5d7735887db8f9b8651830e72
kwerer <josephlailailai247@gmail.com>
2023-01-29 06:26:30 +0800

    added error handling + only can use options to answer questions

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -156,3 +156,13 @@ bot.on("callback_query", (query) => {
     bot.sendMessage(process.env.LOCAL_BOT_CHAT_ID, submitOnce);
   }
 });
+
+exports.handler = async (event) => {
+  try {
+    await bot.handleUpdate(JSON.parse(event.body));
+    return { statusCode: 200, body: "" };
+  } catch (e) {
+    console.error("error in handler:", e);
+    return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" };
+  }
+};
